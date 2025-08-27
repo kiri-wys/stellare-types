f02b658878545c66820f25c7cb0d32d447165065
Kiri Wysteria <kiriwysteria@gmail.com>
2025-08-27 16:26:00 -0500

    feat(angle): Change to a more simplified API

--- src/math/angles.rs
+++ src/math/angles.rs
@@ -1,94 +1,37 @@
-use std::marker::PhantomData;
+use crate::math::Decimal;
 
-use crate::math::{Decimal, Scalar};
-
-pub trait AngleUnit {}
-pub struct Radians;
-pub struct Degrees;
-impl AngleUnit for Radians {}
-impl AngleUnit for Degrees {}
-
-pub trait AngleConversions<D>
-where
-    D: Decimal + Scalar<D>,
-{
-    fn num_radians(self) -> D;
-    fn num_degrees(self) -> D;
-
-    fn to_radians(self) -> Angle<D, Radians>;
-    fn to_degrees(self) -> Angle<D, Degrees>;
-}
-
-#[derive(Debug, Clone, Copy)]
-pub struct Angle<D, U>
-where
-    D: Decimal,
-    U: AngleUnit,
-{
-    num: D,
-    _phantom: PhantomData<(D, U)>,
-}
-impl<D, U> Angle<D, U>
-where
-    D: Decimal + Scalar<D>,
-    U: AngleUnit,
-{
-    pub fn radians(rad: D) -> Angle<D, Radians> {
-        Angle {
-            num: rad,
-            _phantom: PhantomData,
-        }
-    }
-    pub fn degrees(deg: D) -> Angle<D, Degrees> {
-        Angle {
-            num: deg,
-            _phantom: PhantomData,
-        }
-    }
-}
-impl<D> AngleConversions<D> for Angle<D, Radians>
+pub trait Angle<D>
 where
     D: Decimal,
 {
-    fn num_radians(self) -> D {
-        self.num
-    }
-
-    fn num_degrees(self) -> D {
-        self.num.to_degrees()
-    }
-
-    fn to_radians(self) -> Angle<D, Radians> {
-        self
-    }
-
-    fn to_degrees(self) -> Angle<D, Degrees> {
-        Angle {
-            num: self.num_degrees(),
-            _phantom: PhantomData,
-        }
-    }
+    fn radians(self) -> D;
+    fn degrees(self) -> D;
 }
-impl<D> AngleConversions<D> for Angle<D, Degrees>
+
+pub struct Radians<D: Decimal>(D);
+pub struct Degrees<D: Decimal>(D);
+
+impl<D> Angle<D> for Radians<D>
 where
     D: Decimal,
 {
-    fn num_radians(self) -> D {
-        self.num.to_radians()
+    fn radians(self) -> D {
+        self.0
     }
 
-    fn num_degrees(self) -> D {
-        self.num
+    fn degrees(self) -> D {
+        self.0.to_degrees()
+    }
+}
+impl<D> Angle<D> for Degrees<D>
+where
+    D: Decimal,
+{
+    fn radians(self) -> D {
+        self.0.to_radians()
     }
 
-    fn to_radians(self) -> Angle<D, Radians> {
-        Angle {
-            num: self.num_degrees(),
-            _phantom: PhantomData,
-        }
-    }
-
-    fn to_degrees(self) -> Angle<D, Degrees> {
-        self
+    fn degrees(self) -> D {
+        self.0
     }
 }
